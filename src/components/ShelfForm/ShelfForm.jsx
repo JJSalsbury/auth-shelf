ffd92cd41aa2a94f9ebe9272043c48421f8adbbc
Ashlyn Shepard <ashlyntshepard@gmail.com>
2022-04-13 16:21:40 -0500

    adds handleClick function to gather user inputs

--- src/components/ShelfForm/ShelfForm.jsx
+++ src/components/ShelfForm/ShelfForm.jsx
@@ -1,17 +1,28 @@
 import {useDispatch} from 'react-redux';
+import {useState} from 'react'
 
 function ShelfForm () {
 
+    const dispatch = useDispatch();
+
+    const [item, setItem] = useState('');
+    const [link, setLink] = useState('');
+
     const handleClick = () => {
-        console.log('you clicked it')
+        console.log('logging item, link: ', item, link);
+        // dispatch({type: 'ADD_NEW_ITEM', payload: {item: item, link: link}});
     }
 
     return(<>
 
-    <input placeholder="Add Item"></input>
-    <input placeholder="Add Link to an Image"></input>
+    <input 
+    onChange={(event) => setItem(event.target.value)}
+    placeholder="Add Item"></input>
+    <input 
+    onChange={(event) => setLink(event.target.value)}
+    placeholder="Add Link to an Image"></input>
 
-    <button>ADD ITEM</button>
+    <button onClick={handleClick}>ADD ITEM</button>
     
     </>)
 }
